92aab6fcf4b1a0d8c3bd1a95dc327a2623f73490
Lisle Mose <lislemose@gmail.com>
2015-11-19 08:54:38 -0500

    Removed stl dependency

--- draino.c
+++ draino.c
@@ -2,19 +2,18 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <queue>
 
-using namespace std;
-
-// Buffer up to 1 MB
+// Buffer up to .5 GB
 #define PAGE_SIZE 4096
-#define NUM_PAGES 256
+#define NUM_PAGES 131072
 
 char* buf;
 
 struct page_queue {
-	queue<char*> pages;
+	char* pages[NUM_PAGES];
 	pthread_mutex_t lock;
+	int front;
+	int size;
 };
 
 page_queue buffered_pages;
@@ -26,7 +25,7 @@ char is_page_available(page_queue& q) {
 	char is_available = 0;
 
 	pthread_mutex_lock(&q.lock);
-	is_available = !q.pages.empty();
+	is_available = q.size > 0;
 	pthread_mutex_unlock(&q.lock);
 
 	return is_available;
@@ -42,8 +41,12 @@ char* dequeue(page_queue& q) {
 
 	if (is_page_available(q)) {
 		pthread_mutex_lock(&(q.lock));
-		page = q.pages.front();
-		q.pages.pop();
+		page = q.pages[q.front];
+		q.front += 1;
+		if (q.front == NUM_PAGES) {
+			q.front = 0;
+		}
+		q.size -= 1;
 		pthread_mutex_unlock(&(q.lock));
 	}
 
@@ -52,7 +55,19 @@ char* dequeue(page_queue& q) {
 
 void enqueue(page_queue& q, char* page) {
 	pthread_mutex_lock(&(q.lock));
-	q.pages.push(page);
+	if (q.size == 0) {
+		q.pages[0] = page;
+		q.front = 0;
+		q.size = 1;
+	} else {
+		int back = q.front + q.size;
+		if (back >= NUM_PAGES) {
+			back -= NUM_PAGES;
+		}
+
+		q.pages[back] = page;
+		q.size += 1;
+	}
 	pthread_mutex_unlock(&(q.lock));
 }
 
@@ -61,12 +76,18 @@ void init() {
 	pthread_mutex_init(&buffered_pages.lock, NULL);
 	pthread_mutex_init(&available_pages.lock, NULL);
 	buf = (char*) calloc(PAGE_SIZE * NUM_PAGES, 1);
+
 	char* page = buf;
 
 	for (int i=0; i<NUM_PAGES; i++) {
-		available_pages.pages.push(page);
+		available_pages.pages[i] = page;
 		page += PAGE_SIZE;
 	}
+
+	available_pages.front = 0;
+	available_pages.size = NUM_PAGES;
+	buffered_pages.front = 0;
+	buffered_pages.size = 0;
 }
 
 int read_page(char* page) {
@@ -114,33 +135,35 @@ void run() {
 
 	// Read first page
 	char* page = dequeue(available_pages);
-
 	int count = read_page(page);
 
+	// Send pages to the output buffer and continue reading until no more pages to read.
 	while (count == PAGE_SIZE) {
 		enqueue(buffered_pages, page);
 		page = dequeue(available_pages);
 		count = read_page(page);
 	}
 
+	// Signal writer thread and wait for it to finish.
 	is_done = 1;
-
 	ret = pthread_join(writer_t, NULL);
-
 	if (ret != 0) {
 		fprintf(stderr, "Draino error joining writer thread: %d\n", ret);
 		exit(ret);
 	}
 
-	// Write the last page
+	// Output any leftovers
 	if (count > 0 && count < PAGE_SIZE) {
 		write_page(page, count);
 	}
 
 	pthread_mutex_destroy(&(buffered_pages.lock));
 	pthread_mutex_destroy(&(available_pages.lock));
+
+	free(buf);
 }
 
 int main(int argc, char** argv) {
 	run();
 }
+
